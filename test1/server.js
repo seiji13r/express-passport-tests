b4512c436c4fb1a8d5d0dc107b1ddb912b0b2826
Seiji Osada <seiji13r@outlook.com>
2019-01-26 00:30:39 -0600

    Server that generates session cookie

--- test1/server.js
+++ test1/server.js
@@ -1,4 +1,7 @@
+// Require Modules
 const express = require("express");
+const uuid = require("uuid/v4")
+const session = require("express-session");
 
 // Create the Server/App Object
 const app = express();
@@ -12,11 +15,43 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 // Set Up the public directory that will serve the static files under /.
 app.use(express.static("public"));
+// Set Up the Session Middleware require("express-session")
+// https://github.com/expressjs/session
+app.use(session({
+  genid: (req) => {
+    // This function is executed when a request is Coming from and unknown client
+    // As it is right now This server will send a Cookie with a Session ID.
+    // The Client will Save It as a Cookie for this site.
+    // The Definition of This function is not Necessary it will automatically return a sessionID
+    // The default value is a function which uses the uid-safe library to generate IDs.
+    console.log('Inside the session middleware');
+    console.log(req.sessionID);
+    return uuid(); // use UUIDs for session IDs require("uuid/v4")
+  },
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // *********  ROUTES  *************
 app.get("/", (req, res) => {
+  // 1rst Connection
+  // client req/no-session-cookie => server trigger session-genid() res/with-session-cookie
+  // 2nd Connection and Subsequent Connections
+  // client req/with-session-cookie => server don't trigger session-genid() res/no-session-cookie
+  // As there's no storage method if the server goes down the next connection will generate another cookie, 
+  // even if the client send its sesion id cookie in the headers.
+  // Use Google Dev Tools [Network->Name->Headers] and [Application/Cookies] to monitor the behaviors.
+  console.log('Inside the homepage callback function');
+  console.log(req.sessionID);
+  
   const client_ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
-  res.send(`Hello World! You have the Following IP Address: [${client_ip}]`);
+  const sessionID = req.sessionID;
+
+  res.send(
+    `Hello World! You have the Following IP Address: [${client_ip}] SessionID:[${sessionID}]`
+  );
+
 });
 
 // Initialize thw Server
